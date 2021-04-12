3fe08f27fafae452538f0199d55ea0aece5106f7
LevaniFireman <levdavidov1@yandex.ru>
2021-04-12 16:40:10 +0300

    циклы

--- js/script.js
+++ js/script.js
@@ -9,14 +9,31 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
+let i = 0;
+do  {
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+          b = prompt('На сколько его оцените', '');
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = +prompt('На сколько его оцените', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = +prompt('На сколько его оцените', '');
-      
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+        i++;
+}
+while (i < 2);
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
 
 console.log(personalMovieDB);
